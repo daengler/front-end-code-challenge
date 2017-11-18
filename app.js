666d0318a54df879cfcc11d688e890351e8d3483
David Engler <dengler@gmail.com>
2017-11-18 11:35:42 -0500

    Enabled CORS on the Express Server ( https://enable-cors.org/server_expressjs.html )

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var app = express();
 var publicPathName = 'public';
 var publicPath = path.join(__dirname, publicPathName);
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(publicPath));
